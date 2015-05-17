875fcaed2edccb012c1974f6b3a17cba2724a781
Razvy000 <razvy000@gmail.com>
2015-05-17 19:30:16 +0300

    first

--- src/Compute_Standard_Deviation_On_The_Fly.java
+++ src/Compute_Standard_Deviation_On_The_Fly.java
@@ -36,6 +36,27 @@ public class Compute_Standard_Deviation_On_The_Fly {
         }
         return Math.sqrt(S / (k - 2)); // if population S/k-1
     }
+    
+    // expand the equaltion
+    public double standardDeviation2(List<Double> list) {
+        
+        // sum(x-mean)^2 = 
+        // sumx^2 - mean^2
+        double mean = 0;
+        double std = 0;
+        for (Double elem : list) {
+            mean += elem;
+        }
+        mean /= list.size();
+
+        for (Double elem : list) {
+            std += elem * elem;
+        }
+        std -= mean*mean;
+        std /= list.size()-1;
+        std = Math.sqrt(std);
+        return std;
+    }
 
     public static void main(String[] args) {
         Compute_Standard_Deviation_On_The_Fly o = new Compute_Standard_Deviation_On_The_Fly();
@@ -46,7 +67,9 @@ public class Compute_Standard_Deviation_On_The_Fly {
 
         double r = o.standardDeviation(b);
         double r2 = o.standardDeviationFly(b);
+        double r3 = o.standardDeviationFly(b);
         System.out.println(r);
         System.out.println(r2);
+        System.out.println(r3);
     }
 }
