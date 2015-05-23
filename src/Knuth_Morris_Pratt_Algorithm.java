fe137db80ce9da69a51451dfc742ab7f18979cf7
Razvy000 <razvy000@gmail.com>
2015-05-23 13:12:57 +0300

    first

--- src/Knuth_Morris_Pratt_Algorithm.java
+++ src/Knuth_Morris_Pratt_Algorithm.java
@@ -3,13 +3,16 @@ public class Knuth_Morris_Pratt_Algorithm {
 
     // https://www.topcoder.com/community/data-science/data-science-tutorials/introduction-to-string-searching-algorithms/
     // http://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
-    int[] T;
+    
+    // T is the ammount of backtracking, continue from m+i-T[i] 
+    // after failed match when comparing S[m + i] to W[i]    
+    int[] T;    
 
     void table(char[] S, char[] W) {
         int pos = 2;    // the current position we are computing in T
         int cnd = 0;    // the zero-based index in W of the next character of the current candidate substring
 
-        T[0] = -1;
+        T[0] = -1;  // we cannot backtrack for W[0] missmatch
         T[1] = 0;
 
         while (pos < W.length) {
@@ -29,6 +32,18 @@ public class Knuth_Morris_Pratt_Algorithm {
             }
         }
     }
+    /*
+Therefore we compile the following table:
+i	0	1	2	3	4	5	6
+W[i]	A	B	C	D	A	B	D
+T[i]	-1	0	0	0	0	1	2
+    
+Another example, more interesting and complex:
+i	00	01	02	03	04	05	06	07	08	09	10	11	12	13	14	15	16	17	18	19	20	21	22	23
+W[i]	P	A	R	T	I	C	I	P	A	T	E		I	N		P	A	R	A	C	H	U	T	E
+T[i]	-1	0	0	0	0	0	0	0	1	2	0	0	0	0	0	0	1	2	3	0	0	0	0	0
+
+    */
 
     int indexOf(char[] S, char[] W) {
         int m = 0;  // the beginning of the current match in S
